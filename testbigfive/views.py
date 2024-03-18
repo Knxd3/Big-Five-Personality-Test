f4eccfb2561afb63bab701dc1ecb7e2e317b94c1
AD <draghicialexandru358@gmail.com>
2024-03-18 22:16:14 +0000

    add form df

--- testbigfive/views.py
+++ testbigfive/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+import pandas as pd
+import json
 
 # Create your views here.
 
 def questionnaire(request):
     ans = request.POST
-    print(ans)
+    # print(request.body)
+    qry_dict = ans.dict()
+    new_dict = dict()
+    for i in qry_dict.keys():
+        if i != 'csrfmiddlewaretoken':
+            new_dict[i] = list(qry_dict[i])
+    df = pd.DataFrame.from_dict(new_dict, orient='index')
+    df = df.reset_index()
+    df['index_id'] = df['index'].str.extract(r'(\d{1,3})\D*$')
+    print(df)
     context = {
         'n': range(1,21),
         'm': range(1,6),
