fbeb030437ed4cfe5d1ee4b41a5e7bc542a5cd4e
AD <draghicialexandru358@gmail.com>
2024-03-27 10:03:48 +0000

    django heroku repo for personality project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn personalityProject.wsgi:aaplication --log-file -
--- personalityProject/settings.py
+++ personalityProject/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import django_heroku
+import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-v)9y+78ewff#6vlpdk@ic_qbwg4ag5w!7))1fk2+o0q#z41n8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'STATIC/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
